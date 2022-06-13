200ceeaa1b72d9cea71ad64e3c284dbae7cea33a
DESKTOP-843VIUE\TRANTAM <tamtv@ebk.vn>
2022-06-13 16:31:56 +0700

    custom format

--- src/src/screens/ErrorScreen.js
+++ src/src/screens/ErrorScreen.js
@@ -1,37 +1,36 @@
 import React from "react";
 import {
-    View,
-    Text,
-    StyleSheet,
+  View,
+  Text,
+  StyleSheet,
 } from 'react-native';
 
 const ErrorScreen = () => {
-
-    return ( 
-            <View style={styles.container}>
-                <Text style={styles.title}>Lỗi</Text>
-                <Text style={styles.content}>Kết nối internet thất bại!</Text>
-            </View>
-    );
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Lỗi</Text>
+      <Text style={styles.content}>Kết nối internet thất bại!</Text>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#F4F9F8',
-        justifyContent:"center",
-        alignItems:"center"
-    },
-    title:{
-        fontSize:40,
-        fontWeight:'500',
-        color:'#545353'
-    },
-    content:{
-        fontSize:28,
-        fontWeight:'500',
-        color:'#545353'
-    }
+  container: {
+    flex: 1,
+    backgroundColor: '#F4F9F8',
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  title: {
+    fontSize: 40,
+    fontWeight: '500',
+    color: '#545353'
+  },
+  content: {
+    fontSize: 28,
+    fontWeight: '500',
+    color: '#545353'
+  }
 
 })
 export default ErrorScreen;
