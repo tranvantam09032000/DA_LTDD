5ebff2690091fdf6a82c5a40ee17533c3bb69c5b
DESKTOP-843VIUE\TRANTAM <tamtv@ebk.vn>
2022-06-15 16:31:17 +0700

    update name newdetail

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import LoginScreen from './src/screens/LoginScreen'
 import HomeScreen from './src/screens/HomeScreen'
 import RegisterScreen from './src/screens/Register'
-import NewDetail from './src/screens/NewDetail';
+import NewDetailScreen from './src/screens/NewDetailScreen';
 import NewsCategory from './src/screens/NewsCategory';
 // import { DrawerContent } from './src/screens/DrawerScreen';
 // import AuthStack from './src/navigation/AboutStack';
@@ -20,7 +20,7 @@ const App = () =>{
         {/* <Stack.Screen name='Login' component={LoginScreen}/>
         <Stack.Screen name='Register' component={RegisterScreen}/> */}
         <Stack.Screen name='Home' component={HomeScreen}/>
-        {/* <Stack.Screen name='NewDetail' component={NewDetail}/> */}
+        {/* <Stack.Screen name='NewDetail' component={NewDetailScreen}/> */}
         <Stack.Screen name='NewsCategory' component={NewsCategory}/>
       </Stack.Navigator>
     </NavigationContainer>
