1367abab0678d474e7ef8c7c46eced284e2a6226
DESKTOP-843VIUE\TRANTAM <tamtv@ebk.vn>
2022-06-15 19:22:49 +0700

    input search

--- src/src/screens/NewsSubcategoryScreen.js
+++ src/src/screens/NewsSubcategoryScreen.js
@@ -1,45 +1,87 @@
-import React from "react";
+import React, { useState } from "react";
 import {
-  View,
-  Text,
-  TextInput,
-  Button,
-  StyleSheet,
-  ScrollView,
+    View,
+    Text,
+    TextInput,
+    Pressable,
+    StyleSheet,
+    ScrollView,
+    Dimensions,
+    Image
 } from 'react-native';
 import PostsScreen from "./PostsScreen";
 
-const NewsSubcategoryScreen = (props, {navigation}) => {
-  const posts = props.route.params.data.posts;
-  const subcategory = props.route.params.data.subcategory;
+const HEIGHT = Dimensions.get('window').height;
+const WIDTH = Dimensions.get('window').width;
 
-  return (
-    <ScrollView>
-      <View style={styles.container}>
-            <View  style={styles.subCategory}>
-              {/* <Text key={'title-subCategory' + index} style={styles.titleCategory}>{subcategory}</Text> */}
-              <PostsScreen news={posts} type={"Home"} countNews={0} navigation={navigation} />
+const NewsSubcategoryScreen = (props, { navigation }) => {
+    const posts = props.route.params.data.posts;
+    const subcategory = props.route.params.data.subcategory;
+    const [keyword, setkeyword] = useState("");
+    return (
+        <ScrollView>
+            <View style={styles.container}>
+                <View style={styles.inputSearch}>
+                    <TextInput
+                        style={styles.input}
+                        onChangeText={setkeyword}
+                        value={keyword}
+                        placeholder="keyword..."
+                    />
+                    <Pressable style={styles.button} onPress={() => { console.log("click") }}>
+                        <Image style={styles.icon} source={require('../sources/images/iconsearch.png')} />
+                    </Pressable>
+                </View>
+                <View style={styles.subCategory}>
+                    {/* <Text key={'title-subCategory' + index} style={styles.titleCategory}>{subcategory}</Text> */}
+                    <PostsScreen news={posts} type={"Home"} countNews={0} navigation={navigation} />
+                </View>
             </View>
-      </View>
-    </ScrollView>
+        </ScrollView>
 
-  );
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#F4F9F8',
-  },
-  subCategory: {
-    marginVertical: 5,
-    marginTop: 20
-  },
-  titleCategory: {
-    textAlign: 'center',
-    marginVertical: 5,
-    fontSize: 28,
-    fontWeight: "500"
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#F4F9F8',
+    },
+    subCategory: {
+        marginVertical: 5,
+        marginTop: 20
+    },
+    inputSearch: {
+        flex: 1,
+        marginVertical: 30,
+        paddingHorizontal: 10,
+    },
+    input: {
+        width: WIDTH * 0.96,
+        height: 50,
+        borderRadius: 10,
+        borderWidth: 2,
+        padding: 10,
+        borderColor: '#e1e4ea',
+        fontSize: 18,
+        position: 'relative'
+    },
+    button: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        position: 'absolute',
+        width: 60,
+        height: 50,
+        right: 5,
+        top: 0,
+        borderRadius: 10,
+        borderColor: '#e1e4ea',
+    },
+    titleCategory: {
+        textAlign: 'center',
+        marginVertical: 5,
+        fontSize: 28,
+        fontWeight: "500"
+    },
 })
 export default NewsSubcategoryScreen;
