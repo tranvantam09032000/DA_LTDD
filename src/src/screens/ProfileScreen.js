e09b586d13e40d70131467c8bb8f4b2e7bc5f516
Jerry <ngkhanhtoan0902@gmail.com>
2022-06-20 15:46:12 +0700

    update

--- src/src/screens/ProfileScreen.js
+++ src/src/screens/ProfileScreen.js
@@ -14,12 +14,11 @@ const Profile = () => {
         <View style={styles.container}>
           <View style={styles.header}>
             <View style={styles.headerContent}>
-                <Image style={styles.avatar}
-                  source={{uri: 'https://bootdey.com/img/Content/avatar/avatar6.png'}}/>
+                <Image style={styles.avatar} source={require('../sources/images/logo.png')}/>
 
                 <Text style={styles.name}>Nguyễn Khánh Toàn</Text>
-                <Text style={styles.userInfo}>0306181081@caothang.edu.vn </Text>
-                <Text style={styles.userInfo}>Florida </Text>
+                <Text style={styles.usermssv}>0306181081@caothang.edu.vn </Text>
+                <Text style={styles.userclass}>CDTH18PMA </Text>
             </View>
           </View>
         </View>
@@ -30,6 +29,12 @@ const Profile = () => {
 export default Profile
 
 const styles = StyleSheet.create({
+ 
+  header:{
+    backgroundColor: '#E0F0F8',
+    height:200,
+  },
+ 
  
 });
 
