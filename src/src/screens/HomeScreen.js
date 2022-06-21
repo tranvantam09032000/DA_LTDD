9b08f189760046f2a51e671b4f0ec3afb02912cc
DESKTOP-843VIUE\TRANTAM <tamtv@ebk.vn>
2022-06-21 17:20:58 +0700

    view and like new

--- src/src/screens/HomeScreen.js
+++ src/src/screens/HomeScreen.js
@@ -14,54 +14,6 @@ import PostsScreen from "./PostsScreen";
 
 
 const HomeScreen = ({ navigation, props}) => {
-    // const newsApi = [
-    //     {
-    //         title:'Bài viết số 1',
-    //         content:'Bài viết số 1...',
-    //         type:'datn',
-    //         view:5,
-    //         like:5,
-    //         image:'https://soliloquywp.com/wp-content/uploads/2013/05/action-backlit-beach-1046896-1200x450_c.jpg',
-    //         created:1655624119
-    //     },
-    //     {
-    //         title:'Bài viết số 2',
-    //         content:'Bài viết số 2...',
-    //         type:'datn',
-    //         view:10,
-    //         like:15,
-    //         image:'https://jssors8.azureedge.net/demos/image-slider/img/px-beach-daylight-fun-1430675-image.jpg',
-    //         created:1655627719
-    //     },
-    //     {
-    //         title:'Bài viết số 3',
-    //         content:'Bài viết số 3...',
-    //         type:'datn',
-    //         view:2,
-    //         like:1,
-    //         image:'https://jssors8.azureedge.net/demos/image-slider/img/px-beach-daylight-fun-1430675-image.jpg',
-    //         created:1655541319
-    //     },
-    //     {
-    //         title:'Bài viết số 4',
-    //         content:'Bài viết số 4...',
-    //         type:'datn',
-    //         view:60,
-    //         like:50,
-    //         image:'https://wowslider.com/sliders/demo-51/data1/images/car.jpg',
-    //         created:1655467879
-    //     },
-    //     {
-    //         title:'Bài viết số 5',
-    //         content:'Bài viết số 5...',
-    //         type:'datn',
-    //         view:30,
-    //         like:30,
-    //         image:'https://mdbootstrap.com/img/Photos/Slides/img%20(9).jpg',
-    //         created:1655726400
-    //     },
-
-    // ]
     const [news, setnews] = useState([]);
     const [latestNews, setlatestNews] = useState([]);
     const [viewedNews, setviewedNews] = useState([]);
@@ -99,7 +51,7 @@ const HomeScreen = ({ navigation, props}) => {
     return ( 
             <ScrollView>
                 <View style={styles.container}>
-                    <SliderScreen news={latestNews.slice(1)} type={"NewDetail"} navigation={navigation}/>
+                    <SliderScreen news={latestNews.slice(0,3)} type={"NewDetail"} navigation={navigation}/>
                     <Text style={styles.titleNewPost}>Bài viết mới nhất</Text>
                     <PostsScreen news={latestNews} type={"NewDetail"}  navigation={navigation} />
                     <Text style={styles.titleNewView}>Bài viết quan tâm nhiều</Text>
