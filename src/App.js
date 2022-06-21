bf5cf0ec10f297c794890703ff7d3fbd9af0667d
Jerry <ngkhanhtoan0902@gmail.com>
2022-06-21 14:04:00 +0700

    update

--- src/App.js
+++ src/App.js
@@ -21,13 +21,13 @@ const App = ( props) =>{
     <NavigationContainer>
       <Stack.Navigator screenOptions={{
           headerShown: false}}>
-        {/* <Stack.Screen name='Login' component={LoginScreen}/>
-        <Stack.Screen name='Register' component={RegisterScreen}/> */}   
-        {/* <Stack.Screen name='Home' component={DrawerComponent} /> */}
-        {/* <Stack.Screen name='NewDetail' component={NewDetailScreen}/> */}
-        {/* <Stack.Screen name='NewsCategory' component={NewsCategory}/>
-        <Stack.Screen name='NewsSubcategoryScreen' component={NewsSubcategoryScreen}/>
-        <Stack.Screen name='NewDetail' component={NewDetailScreen}/> */}
+        <Stack.Screen name='Login' component={LoginScreen}/>
+        <Stack.Screen name='Register' component={RegisterScreen}/>  
+        <Stack.Screen name='Home' component={DrawerComponent} />
+        {/* <Stack.Screen name='NewDetail' component={NewDetailScreen}/>  */}
+        {/* <Stack.Screen name='NewsCategory' component={NewsCategory}/> */}
+        {/* <Stack.Screen name='NewsSubcategoryScreen' component={NewsSubcategoryScreen}/> */}
+        <Stack.Screen name='NewDetail' component={NewDetailScreen}/>
         <Stack.Screen name='Profile' component={Profile}/>
         <Stack.Screen name='EditProfile' component={EditProfile}/>
       </Stack.Navigator>
